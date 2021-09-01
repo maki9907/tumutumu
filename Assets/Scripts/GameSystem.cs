e79321c3d9e37371107af6013daa1ea6e1f1facc
maki9907 <kouta.uec18@gmail.com>
2021-09-01 19:00:04 +0900

    ツムが同じ種類で3つ以上のときに消す昨日の実装

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class GameSystem : MonoBehaviour
 {
     [SerializeField] BallGenerator ballGenerator;
+    [SerializeField] List<Ball> removeBalls;
+    Ball currentBall;
+
     bool isDragging;
+
     void Start()
     {
         StartCoroutine(ballGenerator.Spawn(40));
@@ -28,26 +32,90 @@ public class GameSystem : MonoBehaviour
 
     void OnDragBegin()
     {
-        Debug.Log("start drag");
+        //Debug.Log("start drag");
         //マウスによるオブジェクトの判定
         //Raycastを使う
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit   = Physics2D.Raycast(mousePosition, Vector2.zero);
+        RaycastHit2D hit2D = Physics2D.Raycast(mousePosition, Vector2.zero);
+
         //ヒットしたオブジェクトがあり、それがBallスクリプトを持っているかの判定
-        if (hit && hit.collider.GetComponent<Ball>())
+        if (hit2D && hit2D.collider.GetComponent<Ball>())
         {
-            Debug.Log("hit obect");
+            //Debug.Log("hit obect");
+            //Ballクラスでオブジェクトを持ってくる
+            Ball ball = hit2D.collider.GetComponent<Ball>();
+            AddRemoveBall(ball);
+            isDragging = true;
         }
     }
 
     void OnDragging()
     {
-        Debug.Log("draggin now");
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit2D = Physics2D.Raycast(mousePosition, Vector2.zero);
 
+        //ヒットしたオブジェクトがあり、それがBallスクリプトを持っているかの判定
+        if (hit2D && hit2D.collider.GetComponent<Ball>())
+        {
+            //Debug.Log("hit obect");
+            //Ballクラスでオブジェクトを持ってくる
+            Ball ball = hit2D.collider.GetComponent<Ball>();
+            //ballオブジェクト間の距離が1.5f以下のときにリストにballを追加していく
+            float distance = Vector2.Distance(ball.transform.position, currentBall.transform.position);
+            if(distance < 1.5f)
+            {
+                //ballオブジェクトのid(種類)が同じ時のみリストに追加する
+                if(ball.id == currentBall.id)
+                {
+                    AddRemoveBall(ball);
+                }
+            }
+        }
     }
 
     void OnDragEnd()
     {
-        Debug.Log("end drag");
+        //もしballが3つ以上つながっていたらballを消す
+        if(removeBalls.Count >= 3)
+        {
+            for(int i = 0; i < removeBalls.Count; i++)
+            {
+                Destroy(removeBalls[i].gameObject);
+            }
+        }
+        else
+        {
+            for(int i = 0; i < removeBalls.Count; i++)
+            {
+                Ball ball = removeBalls[i];
+                //色をもとに戻す
+                RefreshColor(ball);
+            }
+
+        }       
+        //リストの中身を空にする
+        removeBalls.Clear();
+        isDragging = false;
+    }
+
+    void AddRemoveBall(Ball ball)
+    {
+        currentBall = ball;
+        if (!removeBalls.Contains(ball))
+        {
+            removeBalls.Add(ball);
+            //ballの色を薄くする
+            Color ballColor = ball.GetComponent<SpriteRenderer>().color;
+            ballColor.a = 0.5f;
+            ball.GetComponent<SpriteRenderer>().color = ballColor;
+        }
+    }
+
+    void RefreshColor(Ball ball)
+    {
+        //ballの色を濃くする
+        Color ballColor = ball.GetComponent<SpriteRenderer>().color;
+        ballColor.a = 1.0f;
+        ball.GetComponent<SpriteRenderer>().color = ballColor;
     }
 }
